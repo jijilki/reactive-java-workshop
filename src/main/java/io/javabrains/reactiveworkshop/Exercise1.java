d85f0a2daf654fb9d0a205538ea3d692c629ad9d
Jijil Kakkadathu <jijilki@gmail.com>
2022-11-06 10:56:25 +0000

    Stream excerises Excercise 1 done

--- src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
@@ -1,5 +1,8 @@
 package io.javabrains.reactiveworkshop;
 
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 public class Exercise1 {
 
     public static void main(String[] args) {
@@ -7,24 +10,38 @@ public class Exercise1 {
         // Use StreamSources.intNumbersStream() and StreamSources.userStream()
 
         // Print all numbers in the intNumbersStream stream
-        // TODO: Write code here
+
+
+        StreamSources.intNumbersStream().forEach(x -> System.out.println(x));
+        StreamSources.intNumbersStream().forEach(System.out::println);
 
         // Print numbers from intNumbersStream that are less than 5
-        // TODO: Write code here
+
+        StreamSources.intNumbersStream().filter(x -> x < 5).forEach(System.out::println);
 
         // Print the second and third numbers in intNumbersStream that's greater than 5
         // TODO: Write code here
 
+        //StreamSources.intNumbersStream().filter(x -> x > 5).collect(Collectors.toList());
+
         //  Print the first number in intNumbersStream that's greater than 5.
         //  If nothing is found, print -1
-        // TODO: Write code here
+
+        System.out.println(StreamSources.intNumbersStream().filter(x -> x > 5).findFirst().orElse(-1));
 
         // Print first names of all users in userStream
-        // TODO: Write code here
+
+
+        StreamSources.userStream().forEach(x -> System.out.println(x.getFirstName()));
 
         // Print first names in userStream for users that have IDs from number stream
-        // TODO: Write code here
 
+
+        System.out.println(StreamSources.userStream().filter(user -> StreamSources.intNumbersStream().toList().contains(user.getId())).collect(Collectors.toList()));
+
+        Predicate<User> predicate = user -> StreamSources.intNumbersStream().toList().contains(user.getId());
+
+        System.out.println(StreamSources.userStream().filter(predicate).collect(Collectors.toList()));
     }
 
 }
